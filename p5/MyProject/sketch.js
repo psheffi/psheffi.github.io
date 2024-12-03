99aa89ecd655f43ef8ff0b2c494d5024432f9ac3
psheffi <pryce.sheffield@stu.bmcc.cuny.edu>
2024-12-03 16:19:58 -0500

    Add files via upload

---
+++ p5/MyProject/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+ // put setup code here
+  createCanvas(displayWidth,displayHeight);
+  background('pink');
+
+
+}
+function draw() {
+  // put drawing code 
+
+  line (30,20,85,75)
+
+
+//style the line.
+stroke ('white') ;
+
+describe ()
+
+if(mouseIsPressed){
+  circle(mouseX,mouseY,2,1,);
+}
+else{
+  line(mouseX,mouseY,30,20,85,75);
+}
+}
